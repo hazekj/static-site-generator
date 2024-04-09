bb37bc8f4ab535eb575ea59763a87678e75584c5
hazekj <hazekj@gmail.com>
2024-04-08 23:18:23 -0600

    add test cases for LeafNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -9,6 +9,16 @@ class TestHTMLNode(unittest.TestCase):
             "div", "Hello", None, {"id": "first", "href": "https://www.me.com"}
         )
         self.assertEqual(node.props_to_html(), ' id="first" href="https://www.me.com"')
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("a", "Click here", {"href": "www.me.com"})
+        self.assertEqual(node.to_html(), '<a href="www.me.com">Click here</a>')
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Rawr text")
+        self.assertEqual(node.to_html(), "Rawr text")
 
 
 if __name__ == "__main__":
